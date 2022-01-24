a8a4c8fe26e8eda1d7eecb06a8be883620cbbb84
faisoabdirisak <faisoabdirisak@gmail.com>
2022-01-24 09:01:05 +0300

    it will add user save method

--- password_test.py
+++ password_test.py
@@ -20,6 +20,19 @@ class TestClass(unittest.TestCase):
         self.assertEqual(self.new_user.password, "faska12")    
 
 
+    def test_save_user(self):
+        """
+        test case to test if a new user instance has been saved into the User list
+
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+
+
+
+
+
+
 
 
 
