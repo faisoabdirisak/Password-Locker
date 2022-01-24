291e2a08ada1e9bba609e8731f5f6e2a6ca4eb7c
faisoabdirisak <faisoabdirisak@gmail.com>
2022-01-24 08:40:32 +0300

    it will add test init method

--- password_test.py
+++ password_test.py
@@ -11,3 +11,17 @@ class TestClass(unittest.TestCase):
         Set up method to run before each test cases.
         """
         self.new_user =  User("faska-haji", "faska12")
+
+    def test_init(self):
+        """
+        test case to chek if the object has been initialized correctly
+        """
+        self.assertEqual(self.new_user.username, "faska-haji")
+        self.assertEqual(self.new_user.password, "faska12")    
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
