24c90b3460b1216f44529a0b9e5e4c2e2440ca99
faisoabdirisak <faisoabdirisak@gmail.com>
2022-01-24 08:28:08 +0300

    it will add test class and setip method

--- password_test.py
+++ password_test.py
@@ -1,2 +1,13 @@
 import unittest
 from password import User
+
+class TestClass(unittest.TestCase):
+    """
+    A test class that defines test cases for the user class.
+    """
+
+    def setUp(self):
+        """
+        Set up method to run before each test cases.
+        """
+        self.new_user =  User("faska-haji", "faska12")
